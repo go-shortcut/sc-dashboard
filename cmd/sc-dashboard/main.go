3a2c0d51cd1c92f1012ac4abf79f3a967aeb6e74
Anatolii Vorona <vorona.tolik@gmail.com>
2021-09-24 15:10:36 +0200

    remove hardcoded repo

--- cmd/sc-dashboard/main.go
+++ cmd/sc-dashboard/main.go
@@ -16,20 +16,23 @@ func main() {
 		fmt.Println("GITHUB_ACCESS_TOKEN environ is required")
 		os.Exit(1)
 	}
+	githubRepoName := os.Getenv("GITHUB_REPO_NAME")
+	if githubAccessToken == "" {
+		fmt.Println("GITHUB_REPO_NAME environ is required")
+		os.Exit(1)
+	}
+	githubOwnerName := os.Getenv("GITHUB_OWNER_NAME")
+	if githubAccessToken == "" {
+		fmt.Println("GITHUB_OWNER_NAME environ is required")
+		os.Exit(1)
+	}
 
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubAccessToken})
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	repos, _, err := client.Repositories.List(ctx, "", nil)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	hardcodedRepo := repos[3] // TODO
-	owner := *hardcodedRepo.Owner.Login
-	repo := *hardcodedRepo.Name
-	comp, _, err := client.Repositories.CompareCommits(ctx, owner, repo, "master", "premaster2", nil)
+	comp, _, err := client.Repositories.CompareCommits(ctx, githubOwnerName, githubRepoName, "master", "premaster2", nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -38,8 +41,8 @@ func main() {
 	for _, c := range comp.Commits {
 		allChangeMessages += *c.Commit.Message
 	}
-	//println(allChangeMessages)
-	idSHniki := map[string]bool{}
+
+	scStoryIds := make(map[string]interface{})
 	var chPatterns = []string{
 		`/ch([0-9]+)/`,
 		`/sc-([0-9]+)/`,
@@ -51,16 +54,21 @@ func main() {
 		reCHinBracket := regexp.MustCompile(pattern)
 		listReCHinBracket := reCHinBracket.FindAllStringSubmatch(allChangeMessages, -1)
 		for _, i := range listReCHinBracket {
-			idSHniki[i[1]] = true
+			scStoryIds[i[1]] = nil
 		}
-		fmt.Printf("%s # %d # %+v\n", pattern, len(idSHniki), idSHniki)
 	}
 
-	var resultStroyIds []string
-	for stringId := range idSHniki {
-		resultStroyIds = append(resultStroyIds, stringId)
+	fmt.Printf("# %d # %+v\n", len(scStoryIds), GetSliceOfStringKeysFromMap(scStoryIds))
+
+}
+
+func GetSliceOfStringKeysFromMap(m map[string]interface{}) []string {
+	keys := make([]string, len(m))
+
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
 	}
-
-	fmt.Printf("# %d # %+v\n", len(resultStroyIds), resultStroyIds)
-
+	return keys
 }
