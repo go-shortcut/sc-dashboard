3a65359ac59e93f236534d0b4a2d0892ae8ca8ae
Anatolii Vorona <vorona.tolik@gmail.com>
2021-09-24 17:13:20 +0200

    init 3

--- cmd/sc-dashboard/main.go
+++ cmd/sc-dashboard/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,27 +19,53 @@ func main() {
 		fmt.Println("GITHUB_ACCESS_TOKEN environ is required")
 		os.Exit(1)
 	}
-	githubRepoName := os.Getenv("GITHUB_REPO_NAME")
-	if githubAccessToken == "" {
-		fmt.Println("GITHUB_REPO_NAME environ is required")
-		os.Exit(1)
-	}
-	githubOwnerName := os.Getenv("GITHUB_OWNER_NAME")
-	if githubAccessToken == "" {
-		fmt.Println("GITHUB_OWNER_NAME environ is required")
-		os.Exit(1)
-	}
+
 	token := os.Getenv("SHORTCUT_API_TOKEN")
 	if token == "" {
 		fmt.Println("SHORTCUT_API_TOKEN environ is required")
 		os.Exit(1)
 	}
+
+	GithubRepository := os.Getenv("GITHUB_REPOSITORY")
+	var githubOwnerName, githubRepoName string
+	if GithubRepository == "" {
+		fmt.Println("GITHUB_REPOSITORY environ is required")
+		fmt.Println("GITHUB_REPOSITORY\tThe owner and repository name. For example, octocat/Hello-World.")
+		os.Exit(1)
+	} else {
+		splitedGithubRepository := strings.Split(GithubRepository, "/")
+		if len(splitedGithubRepository) != 2 {
+			fmt.Println("Failed split GITHUB_REPOSITORY to the owner and repository name.")
+			os.Exit(1)
+		}
+		githubOwnerName, githubRepoName = splitedGithubRepository[0], splitedGithubRepository[1]
+	}
+
+	GithubBaseRef := os.Getenv("GITHUB_BASE_REF")
+	GithubHeadRef := os.Getenv("GITHUB_HEAD_REF")
+	if len(GithubBaseRef)*len(GithubHeadRef) == 0 {
+		// GITHUB_BASE_REF=refs/heads/master GITHUB_HEAD_REF=refs/heads/premaster2
+		fmt.Printf("Bad ref variables: GITHUB_BASE_REF=%s, GITHUB_HEAD_REF=%s.\n", GithubBaseRef, GithubHeadRef)
+		os.Exit(1)
+
+	}
+
+	ShortcutAddLabel := os.Getenv("SHORTCUT_ADD_LABEL")
+	ShortcutDelLabel := os.Getenv("SHORTCUT_DEL_LABEL")
+	if len(ShortcutAddLabel)+len(ShortcutDelLabel) == 0 {
+		// SHORTCUT_ADD_LABEL="master marty" SHORTCUT_DEL_LABEL="premaster2 marty"
+		fmt.Println("At least one variable must be set: SHORTCUT_ADD_LABEL, SHORTCUT_DEL_LABEL.")
+		os.Exit(1)
+
+	}
+
+	// all env variables are checked, let's go
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubAccessToken})
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	comp, _, err := client.Repositories.CompareCommits(ctx, githubOwnerName, githubRepoName, "master", "premaster2", nil)
+	comp, _, err := client.Repositories.CompareCommits(ctx, githubOwnerName, githubRepoName, GithubBaseRef, GithubHeadRef, nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -66,22 +93,23 @@ func main() {
 
 	storyIds := GetKeysAsInt64Slice(scStoryIds)
 	fmt.Printf("# %d # %d # %+v\n", len(scStoryIds), len(storyIds), storyIds)
-
-	GithubBranchName := "premaster2"
+	//os.Exit(1)
 
 	shortcutClient := shortcutclient.New(token)
 	//shortcutClient.HTTPClient.Timeout = 30 * time.Second
 	//shortcutClient.Debug = true
 	playload := shortcutclient.UpdateMultipleStoriesParams{
 		StoryIds: storyIds,
-		LabelsAdd: []shortcutclient.CreateLabelParams{
-			{Name: GithubBranchName, Color: "#0000FF"},
-		},
 	}
 
-	if GithubBranchName == "master" {
+	if ShortcutAddLabel != "" {
+		playload.LabelsAdd = []shortcutclient.CreateLabelParams{
+			{Name: ShortcutAddLabel, Color: "#0000FF"},
+		}
+	}
+	if ShortcutDelLabel != "" {
 		playload.LabelsRemove = []shortcutclient.CreateLabelParams{
-			{Name: "premaster2", Color: "#FF0000"},
+			{Name: ShortcutDelLabel},
 		}
 	}
 
